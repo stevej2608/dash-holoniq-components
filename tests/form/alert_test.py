518e7ad7d8cdde4187ba9a65e806566f71b9bdd1
Steve Jones <jonesst2608@gmail.com>
2021-09-17 09:05:34 +0100

    Refactoring

---
+++ tests/form/alert_test.py
@@ -0,0 +1,26 @@
+
+from tests.form import USER_NAME, USER_PASSWORD
+
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+def test_alert_component(duo):
+
+    # Open the sign in form
+
+    duo.server_url = duo.server_url + "/signin"
+
+    # Try to submit empty form
+
+    btn = duo.find_element("#form_signin_btn")
+    btn.click()
+
+    assert duo.wait_for_text_to_equal("#flash", "Enter your name", timeout=20)
+
+    # Try to submit user name with no password
+
+    name=duo.find_element("#user_name")
+    name.send_keys(USER_NAME)
+
+    btn.click()
+
+    assert duo.wait_for_text_to_equal("#flash", "Password must be at least 8 characters", timeout=20)
